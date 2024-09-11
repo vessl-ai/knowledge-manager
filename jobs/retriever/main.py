b2baa328b9d983bd2bb53bb10f679597f86ea967
nsd9696 <nsd26534613@gmail.com>
2024-09-10 17:32:53 -0700

    change to general vectodb_client

--- jobs/retriever/main.py
+++ jobs/retriever/main.py
@@ -12,18 +12,19 @@ from chromadb.config import Settings
 
 app = FastAPI()
 
-class ChromaClient(BaseModel):
+class VectorDBClient(BaseModel):
+    type: str
     host: str
     port: int
-    chroma_client_auth_provider: str | None = None
-    chroma_client_auth_credentials: str | None = None
+    auth_provider: str | None = None
+    auth_credentials: str | None = None
 
 class EmbeddingModel(BaseModel):
     type: str
     model: str
 
 class Query(BaseModel):
-    chroma_client: ChromaClient
+    vectordb_client: VectorDBClient
     embedding_model: EmbeddingModel
     collection_name: str
     prompt: str
@@ -36,18 +37,21 @@ def read_root():
 
 @app.post("/query")
 async def retrieve(query: Query):
-    if query.chroma_client.chroma_client_auth_provider != None and query.chroma_client.chroma_client_auth_credentials != None:
+    if query.vectordb_client.type != "chroma":
+        return None
+    
+    if query.vectordb_client.auth_provider != None and query.vectordb_client.auth_credentials != None:
         client = chromadb.HttpClient(
-            host=query.chroma_client.host,
-            port=query.chroma_client.port,
+            host=query.vectordb_client.host,
+            port=query.vectordb_client.port,
             settings=Settings(
-                chroma_client_auth_provider=query.chroma_client.chroma_client_auth_provider,
-                chroma_client_auth_credentials=query.chroma_client.chroma_client_auth_credentials),
+                chroma_client_auth_provider=query.vectordb_client.auth_provider,
+                chroma_client_auth_credentials=query.vectordb_client.auth_credentials),
         )
     else:
         client = chromadb.HttpClient(
-            host=query.chroma_client.host,
-            port=query.chroma_client.port,
+            host=query.vectordb_client.host,
+            port=query.vectordb_client.port,
         )
     embeddings = None
     if query.embedding_model.type == "openai":
